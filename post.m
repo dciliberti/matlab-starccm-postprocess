fadd090b3e3e0f7907eecfb32f464b62f06c6a0c
Danilo Ciliberti <dancili@gmail.com>
2020-03-11 09:53:35 +0100

    added filepath and description

--- post.m
+++ post.m
@@ -1,4 +1,32 @@
+% Calls STAR-CCM+ in batch with a macro to get and plot the desired reports
+% versus an independent variable. Useful to list and plot data from a
+% design sweep like an airfoil investigated at several angles of attack,
+% where each sim file is an angle of attack. Works with Microsoft Windows.
+%
+% It is assumed that the sim file name has the numeric independent variable
+% in the last characters. MATLAB will try to get the 'prefix' of this file
+% name, i.e. the root of the name without the independent variable value.
+% For instance:
+%
 % mysimfile_alpha0.0.sim  -->  prefix = mysimfile_alpha
+% or
+% mysimfile_alpha0.sim  -->  prefix = mysimfile_alpha
+%
+% This script will work well with the last characters as floating point
+% digits, even with zero decimal positions. The script will ask the user to
+% check if the prefix has been correctly captured, otherwise it asks the
+% user to write the prefix.
+%
+% Then the script will ask the user to choose the sim files from which 
+% extract all the reports with the JAVA macro 'Report_to_csv.java'. The sim
+% files are all those with the same prefix previously defined, in the
+% folder initially selected, so that it should be easy to get the files of
+% interest even if the folder is populated with many and different file
+% types.
+%
+% Finally, the script ask the user which report to plot versus the
+% independent variable, from a list of all extracted reports. Sparse
+% selection (e.g. non-contiguous angles of attack) is also permitted.
 
 close all; clearvars; clc
 
@@ -36,9 +64,9 @@ indVar = inputdlg('Write the independent variable name (no spaces, no latex symb
         'X-axis label',[1,40],{'variable'});
 
 % Get the desired sim files
-listing = dir([prefix,'*.sim']);
+listing = dir([filePath,prefix,'*.sim']);
 [indx,~] = listdlg('ListString',{listing.name},...
-    'PromptString','Select sim files');
+    'PromptString','Select sim files','ListSize',[450,250]);
 
 % Check variable digit of precision
 var = simName(position+1:end);
@@ -62,29 +90,30 @@ array = sort(array);
 pod = '2jHU+QkwqexqrAOdVZ6ZzQ';
 disp('Report generation...')
 for i = 1:length(array)
-    fileName = [prefix,num2str(array(i),formatString)];
-    % Maybe add filePath with fileName...
+    fileName = [filePath,prefix,num2str(array(i),formatString)];
     star = ['starccm+.exe ', fileName, '.sim -batch "Report_to_csv.java" -power -podkey ', pod, ' -licpath 1999@flex.cd-adapco.com'];
     dos(star);
 end
 
 %% Visualizzazione risultati
 
-% Get variables name and number
-csvName = [prefix,num2str(array(1),formatString),'_report.csv'];
+% Get variables name and number from the first file of the list
+csvName = [filePath,prefix,num2str(array(1),formatString),'_report.csv'];
 aero = readtable(csvName,'ReadRowNames',true);
 
-% Ask user which variable plot
+% Ask user which variable get and plot
 [indx,~] = listdlg('ListString',aero.Row,...
-    'PromptString','Select variables to plot');
+    'PromptString','Select variables to plot','ListSize',[250,250]);
 
+% Initialize variables array
 varNumber = length(indx);
 varName = aero.Row;
 var = zeros(length(array),varNumber);
 
+% Cycle over the selected sim files to get the desired variables
 for i = 1:length(array)
-    fileName = [prefix,num2str(array(i),formatString),'_report.csv'];
-    aero = readtable(fileName,'ReadRowNames',true);
+    csvName = [filePath,prefix,num2str(array(i),formatString),'_report.csv'];
+    aero = readtable(csvName,'ReadRowNames',true);
     
     for j = 1:varNumber
         var(i,j) = aero{indx(j),1};
@@ -105,7 +134,7 @@ for i = 1:size(var,2)
 end
 disp(out)
 
-% Plot data
+% Plot data: selected variables versus the independent variable
 for i = 1:size(var,2)
     figure
     plot(array,var(:,i),'o-','LineWidth',2)
